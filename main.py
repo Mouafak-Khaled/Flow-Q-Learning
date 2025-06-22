03dc25ebd185d6e0b586323cc7de91fe41c04041
Mohamed <mohamedhatemibrahim@gmail.com>
2025-06-22 17:59:44 +0200

    adding run script

---
+++ main.py
@@ -0,0 +1,136 @@
+import os
+
+import json
+import random
+import time
+
+import numpy as np
+import tqdm
+import wandb
+from absl import app, flags
+from ml_collections import config_flags
+
+from fql.agents.fql import FQLAgent
+from fql.envs.env_utils import make_env_and_datasets
+from fql.utils.datasets import Dataset, ReplayBuffer
+from fql.utils.evaluation import evaluate
+from fql.utils.flax_utils import save_agent
+from fql.utils.log_utils import CsvLogger, get_exp_name, get_flag_dict, setup_wandb
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('run_group', 'Debug', 'Run group.')
+flags.DEFINE_integer('seed', 0, 'Random seed.')
+flags.DEFINE_string('env_name', 'cube-double-play-singletask-v0', 'Environment (dataset) name.')
+flags.DEFINE_string('save_dir', 'exp/', 'Save directory.')
+
+flags.DEFINE_integer('steps', 1000000, 'Number of offline steps.')
+flags.DEFINE_integer('buffer_size', 2000000, 'Replay buffer size.')
+flags.DEFINE_integer('log_interval', 5000, 'Logging interval.')
+flags.DEFINE_integer('eval_interval', 100000, 'Evaluation interval.')
+flags.DEFINE_integer('save_interval', 1000000, 'Saving interval.')
+
+flags.DEFINE_integer('eval_episodes', 50, 'Number of evaluation episodes.')
+flags.DEFINE_integer('video_frame_skip', 3, 'Frame skip for videos.')
+
+flags.DEFINE_float('p_aug', None, 'Probability of applying image augmentation.')
+flags.DEFINE_integer('frame_stack', None, 'Number of frames to stack.')
+
+config_flags.DEFINE_config_file('agent', 'fql/agents/fql.py', lock_config=False)
+
+
+def main(_):
+    # Set up logger.
+    exp_name = get_exp_name(FLAGS.seed)
+    setup_wandb(project='fql', group=FLAGS.run_group, name=exp_name)
+
+    FLAGS.save_dir = os.path.join(FLAGS.save_dir, wandb.run.project, FLAGS.run_group, exp_name)
+    os.makedirs(FLAGS.save_dir, exist_ok=True)
+    flag_dict = get_flag_dict()
+    with open(os.path.join(FLAGS.save_dir, 'flags.json'), 'w') as f:
+        json.dump(flag_dict, f)
+
+    # Make environment and datasets.
+    config = FLAGS.agent
+    env, eval_env, train_dataset, val_dataset = make_env_and_datasets(FLAGS.env_name, frame_stack=FLAGS.frame_stack)
+
+    # Initialize agent.
+    random.seed(FLAGS.seed)
+    np.random.seed(FLAGS.seed)
+
+    # Set up datasets.
+    train_dataset = Dataset.create(**train_dataset)
+    # Use the training dataset as the replay buffer.
+    train_dataset = ReplayBuffer.create_from_initial_dataset(
+        dict(train_dataset), size=max(FLAGS.buffer_size, train_dataset.size + 1)
+    )
+
+    # Set p_aug and frame_stack.
+    for dataset in [train_dataset, val_dataset]:
+        if dataset is not None:
+            dataset.p_aug = FLAGS.p_aug
+            dataset.frame_stack = FLAGS.frame_stack
+
+    # Create agent.
+    example_batch = train_dataset.sample(1)
+
+    agent = FQLAgent.create(
+        FLAGS.seed,
+        example_batch['observations'],
+        example_batch['actions'],
+        config,
+    )
+
+    # Train agent.
+    train_logger = CsvLogger(os.path.join(FLAGS.save_dir, 'train.csv'))
+    eval_logger = CsvLogger(os.path.join(FLAGS.save_dir, 'eval.csv'))
+    first_time = time.time()
+    last_time = time.time()
+
+    expl_metrics = dict()
+    for i in tqdm.tqdm(range(1, FLAGS.steps + 1), smoothing=0.1, dynamic_ncols=True):
+        batch = train_dataset.sample(config['batch_size'])
+        agent, update_info = agent.update(batch)
+
+        # Log metrics.
+        if i % FLAGS.log_interval == 0:
+            train_metrics = {f'training/{k}': v for k, v in update_info.items()}
+            if val_dataset is not None:
+                val_batch = val_dataset.sample(config['batch_size'])
+                _, val_info = agent.total_loss(val_batch, grad_params=None)
+                train_metrics.update({f'validation/{k}': v for k, v in val_info.items()})
+            train_metrics['time/epoch_time'] = (time.time() - last_time) / FLAGS.log_interval
+            train_metrics['time/total_time'] = time.time() - first_time
+            train_metrics.update(expl_metrics)
+            last_time = time.time()
+            wandb.log(train_metrics, step=i)
+            train_logger.log(train_metrics, step=i)
+
+        # Evaluate agent.
+        if FLAGS.eval_interval != 0 and (i == 1 or i % FLAGS.eval_interval == 0):
+            renders = []
+            eval_metrics = {}
+            eval_info, trajs, cur_renders = evaluate(
+                agent=agent,
+                env=eval_env,
+                config=config,
+                num_eval_episodes=FLAGS.eval_episodes,
+                video_frame_skip=FLAGS.video_frame_skip,
+            )
+            renders.extend(cur_renders)
+            for k, v in eval_info.items():
+                eval_metrics[f'evaluation/{k}'] = v
+
+            wandb.log(eval_metrics, step=i)
+            eval_logger.log(eval_metrics, step=i)
+
+        # Save agent.
+        if i % FLAGS.save_interval == 0:
+            save_agent(agent, FLAGS.save_dir, i)
+
+    train_logger.close()
+    eval_logger.close()
+
+
+if __name__ == '__main__':
+    app.run(main)
