d2c8df1e478455b1cd87efac5c94edd8e795362b
Mazen Amria <t.mazenamria@gmail.com>
2025-07-03 14:33:02 +0200

    fix stuff

--- scripts/reproduce-cube.sh
+++ scripts/reproduce-cube.sh
@@ -3,12 +3,14 @@
 #SBATCH --job-name=ReproduceAntSoccerResults_FlowQLearning
 #SBATCH --partition=dllabdlc_gpu-rtx2080
 #SBATCH --mem=16GB
-#SBATCH --output=logs/slurm-%A_%a.out
-#SBATCH --error=logs/slurm-%A_%a.err
+#SBATCH --output=/work/dlclarge2/amriam-fql/logs/slurm-%A_%a.out
+#SBATCH --error=/work/dlclarge2/amriam-fql/logs/slurm-%A_%a.err
 
 # Activate your environment
 source ~/miniconda3/bin/activate
 conda activate fql
 
-# Run the experiment with the current SLURM_ARRAY_TASK_ID
-python reproduce.py --env_name=cube-single-play-singletask-task2-v0 --agent.alpha=300
+python reproduce.py \
+    --env_name=cube-single-play-singletask-task2-v0 --agent.alpha=300 \
+    --save_directory=/work/dlclarge2/amriam-fql/exp/ --data_directory=/work/dlclarge2/amriam-fql/data/ \
+    --number_of_seeds=8 --use_wandb
--- scripts/tune-alpha-antsoccer.sh
+++ scripts/tune-alpha-antsoccer.sh
@@ -3,11 +3,15 @@
 #SBATCH --job-name=TuneAlphaAntSoccer_FlowQLearning
 #SBATCH --partition=dllabdlc_gpu-rtx2080
 #SBATCH --mem=16GB
-#SBATCH --output=logs/slurm-%A_%a.out
-#SBATCH --error=logs/slurm-%A_%a.err
+#SBATCH --output=/work/dlclarge2/amriam-fql/logs/slurm-%A_%a.out
+#SBATCH --error=/work/dlclarge2/amriam-fql/logs/slurm-%A_%a.err
 
 # Activate your environment
 source ~/miniconda3/bin/activate
 conda activate fql
 
-python tune_alpha.py --env_name=antsoccer-arena-navigate-singletask-task4-v0 --agent.discount=0.995 --evaluation_mode --use_wandb
+python tune_alpha.py \
+    --env_name=antsoccer-arena-navigate-singletask-task4-v0 --agent.discount=0.995 \
+    --save_directory=/work/dlclarge2/amriam-fql/exp/ --data_directory=/work/dlclarge2/amriam-fql/data/ \
+    --number_of_seeds=2 --number_of_alphas=10 \
+    --evaluation_mode --use_wandb
