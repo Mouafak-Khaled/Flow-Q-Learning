f1346c8951b191a7bb40bc874c5abfb9376a8be9
Mazen Amria <t.mazenamria@gmail.com>
2025-07-09 15:00:01 +0200

    create new CsvLogger that allows for continuation and stepless logging

--- utils/logger.py
+++ utils/logger.py
@@ -6,8 +6,37 @@ import wandb
 from typing_extensions import Literal
 import yaml
 
-from fql.utils.log_utils import CsvLogger
 from trainer.config import AgentConfig
+
+
+class CsvLogger:
+    """CSV logger for logging metrics to a CSV file."""
+
+    def __init__(self, path):
+        self.path = path
+        self.header = None
+        self.file = None
+        self.disallowed_types = (wandb.Image, wandb.Video, wandb.Histogram)
+
+    def log(self, row, step=None):
+        if step is not None:
+            row['step'] = step
+    
+        if self.file is None:
+            self.file = open(self.path, 'w')
+
+        if self.header is None:
+            self.header = [k for k, v in row.items() if not isinstance(v, self.disallowed_types)]
+            self.file.write(','.join(self.header) + '\n')
+
+        filtered_row = {k: v for k, v in row.items() if not isinstance(v, self.disallowed_types)}
+        self.file.write(','.join([str(filtered_row.get(k, '')) for k in self.header]) + '\n')
+
+        self.file.flush()
+
+    def close(self):
+        if self.file is not None:
+            self.file.close()
 
 
 class Logger:
