6801da191d0da20f6d619e4b6d8da4c65d4302f6
Mouafak Alsaid Hasan <mouafak.hasan@breathment.com>
2025-07-09 15:17:42 +0200

    Implement the changes requireed in the pull request

--- hpo/strategy_evaluator.py
+++ hpo/strategy_evaluator.py
@@ -4,7 +4,7 @@ from typing import List
 from hpo.strategy import HpoStrategy
 from hpo.successive_halving import SuccessiveHalving
 from trainer.config import ExperimentConfig
-from fql.utils.log_utils import CsvLogger
+from utils.logger import CsvLogger
 from pathlib import Path
 
 
@@ -23,8 +23,8 @@ class HpoStrategyEvaluator(HpoStrategy):
         state_dict: dict | None = None,
     ):
         super().__init__(population=population, state_dict=state_dict)
-        log_path = save_directory / env_name / experiment_name / "evaluator.csv"
-        self.logger = CsvLogger(log_path, header=["evaluation_step", "strategy_name", "stopped_at_evaluation"])
+        log_path = save_directory / env_name / "strategies_stopping_times.csv"
+        self.logger = CsvLogger(log_path, header=[ "strategy_name", "experiment_name", "stopped_at"])
 
         STRATEGIES = {
             "successive_halving_0.5": (
@@ -39,7 +39,6 @@ class HpoStrategyEvaluator(HpoStrategy):
 
         if state_dict is not None:
             self.performed_evaluations = state_dict["performed_evaluations"]
-            self.stopped_at = defaultdict(int, state_dict["stopped_at"])
             self.strategies = {
                 name: STRATEGIES[name][0](**STRATEGIES[name][1], state_dict=state_dict)
                 for name, state_dict in state_dict["strategies"].items()
@@ -50,7 +49,6 @@ class HpoStrategyEvaluator(HpoStrategy):
                 name: strategy_class(**kwargs)
                 for name, (strategy_class, kwargs) in STRATEGIES.items()
             }
-            self.stopped_at = defaultdict(int)
 
     def state_dict(self) -> dict:
         """
@@ -61,7 +59,6 @@ class HpoStrategyEvaluator(HpoStrategy):
         """
         return {
             **super().state_dict(),
-            "stopped_at": dict(self.stopped_at),
             "strategies": {
                 name: strategy.state_dict()
                 for name, strategy in self.strategies.items()
@@ -77,7 +74,7 @@ class HpoStrategyEvaluator(HpoStrategy):
             candidate (ExperimentConfig): The candidate for which to update the performance history.
             performance (float): The performance score of the candidate.
         """
-        for name, strategy in self.strategies.items():
+        for strategy in self.strategies.values():
             if candidate in strategy.population:
                 strategy.update(candidate, performance)
 
@@ -94,13 +91,11 @@ class HpoStrategyEvaluator(HpoStrategy):
             old_population = strategy.population
             new_population = strategy.sample()
 
-            for candidate in new_population:
-                if candidate not in old_population:
-                    if self.stopped_at[name] == 0:
-                        self.stopped_at[name] = self.performed_evaluations
-                        self.logger.log({
-                            "evaluation_step": self.performed_evaluations,
-                            "strategy_name": name,
-                            "stopped_at_evaluation": self.performed_evaluations
-                        })
+            for candidate in old_population:
+                if candidate not in new_population:
+                    self.logger.log({
+                        "evaluation_step": self.performed_evaluations,
+                        "strategy_name": name,
+                        "stopped_at_evaluation": self.performed_evaluations
+                    })
         return self.population
