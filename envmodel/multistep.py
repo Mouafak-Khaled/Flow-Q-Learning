be1a50e6ab3b40c90146eefc4d913ed4c3a67ed7
Mazen Amria <t.mazenamria@gmail.com>
2025-07-25 01:25:02 +0200

    fix errors

--- envmodel/multistep.py
+++ envmodel/multistep.py
@@ -7,35 +7,26 @@ from envmodel.baseline import BaselineEnvModel
 
 
 class MultistepEnvModel(nn.Module):
-    cell: BaselineEnvModel
     observation_dimension: int = 28
     action_dimension: int = 5
     hidden_size: int = 128
-
-    def setup(self):
-        self.cell = BaselineEnvModel(
-            observation_dimension=self.observation_dimension,
-            action_dimension=self.action_dimension,
-            hidden_size=self.hidden_size,
-        )
 
     @nn.compact
     def __call__(
         self, observations, actions, **kwargs
     ) -> Tuple[jnp.ndarray, jnp.ndarray]:
-        def scan_fn(carry, inputs):
-            next_observations, terminations = self.cell(
-                observations=carry, actions=inputs
-            )
-            return next_observations, (next_observations, terminations)
-
-        _, (next_observations, terminations) = nn.scan(
-            scan_fn,
+        model = nn.scan(
+            BaselineEnvModel,
             variable_broadcast="params",
             split_rngs={"params": False},
             in_axes=1,
-            out_axes=1,
-            length=observations.shape[1],
-        )(observations, actions)
+            out_axes=1
+        )
+
+        _, (next_observations, terminations) = model(
+            observation_dimension=self.observation_dimension,
+            action_dimension=self.action_dimension,
+            hidden_size=self.hidden_size,
+        )(observations[:, 0, :], actions)
 
         return next_observations, terminations
