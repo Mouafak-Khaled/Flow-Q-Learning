807521ea9e5d62e0e9527084489f39c774ea34ef
Mazen Amria <t.mazenamria@gmail.com>
2025-07-30 00:00:52 +0200

    fix script

--- scripts/train-multistep-antsoccer.sh
+++ scripts/train-multistep-antsoccer.sh
@@ -12,12 +12,12 @@ source ~/miniconda3/bin/activate
 conda activate fql
 
 python train_env_model.py \
-    --env_name=cube-single-play-singletask-task2-v0 --model=multistep --termination_weight=0 --steps=10000 \
+    --env_name=antsoccer-arena-navigate-singletask-task4-v0 --model=multistep --termination_weight=0 --steps=10000 \
     --save_directory=/work/dlclarge2/amriam-fql/exp/ --data_directory=/work/dlclarge2/amriam-fql/data/
 
-python evaluate_env_model.py --env_name=cube-single-play-singletask-task2-v0 --model=multistep --eval_episodes=50 \
+python evaluate_env_model.py --env_name=antsoccer-arena-navigate-singletask-task4-v0 --model=multistep --eval_episodes=50 \
     --save_directory=/work/dlclarge2/amriam-fql/exp/ --data_directory=/work/dlclarge2/amriam-fql/data/
 
-python evaluate_success_rate_correlation.py --env_name=cube-single-play-singletask-task2-v0 --model=multistep \
+python evaluate_success_rate_correlation.py --env_name=antsoccer-arena-navigate-singletask-task4-v0 --model=multistep \
     --save_directory=/work/dlclarge2/amriam-fql/exp/ --data_directory=/work/dlclarge2/amriam-fql/data/ \
     --number_of_seeds=2 --number_of_alphas=20 --eval_episodes=50
